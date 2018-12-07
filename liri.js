20b22ec0cdb35a0b998f4f69988ff1f6879cfb45
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-12-07 10:02:21 -0800

    add initial files

---
+++ liri.js
@@ -0,0 +1,8 @@
+// Require dotenv to read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+// Import keys
+var keys = require("./keys.js");
+
+// Access keys information
+// var spotify = new Spotify(keys.spotify);
